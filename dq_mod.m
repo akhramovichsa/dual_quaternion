7d2ecbdc6130b5d640a7b43507f8f38f11b84e8d
AkhramovichSA <akhramovichsa@gmail.com>
2017-11-03 23:18:51 +0300

    1

--- dq_mod.m
+++ dq_mod.m
@@ -1,6 +1,7 @@
 function out = dq_mod(dq)
 % DUALQUAT_MOD  Calculate the modulus of a dual quaternion.
 
-q_real_norm = sqrt(dq(1)^2 + dq(2)^2 + dq(3)^2 + dq(4)^2);
+q_real_mod = sqrt(dq(1)^2 + dq(2)^2 + dq(3)^2 + dq(4)^2);
 
-out = [q_real_norm, (dq(1)*dq(5) + dq(2)*dq(6) + dq(3)*dq(7) + dq(4)*dq(8))/q_real_norm];
+out = [q_real_mod, ...
+       (dq(1)*dq(5) + dq(2)*dq(6) + dq(3)*dq(7) + dq(4)*dq(8))/q_real_mod];
--- dq_norm.m
+++ dq_norm.m
@@ -1,4 +1,5 @@
 function out = dq_norm(dq)
 % DUALQUAT_NORM Calculate the norm of a dual quaternion.
 
-out = [dq(1)^2 + dq(2)^2 + dq(3)^2 + dq(4)^2, 2*(dq(1)*dq(5) + dq(2)*dq(6) + dq(3)*dq(7) + dq(4)*dq(8))];
+out = [dq(1)^2 + dq(2)^2 + dq(3)^2 + dq(4)^2, ...
+       dq(1)*dq(5) + dq(2)*dq(6) + dq(3)*dq(7) + dq(4)*dq(8)];
