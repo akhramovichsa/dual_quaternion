f1c5c5c13c5f95125a04b1631ed9037c78433842
AkhramovichSA <akhramovichsa@gmail.com>
2017-11-03 23:21:17 +0300

    system coordinate rotation matrix

---
+++ nsk2ssk.m
@@ -0,0 +1,14 @@
+function M = nsk2ssk(gamma, psi, theta)
+    sin_gamma = sin(gamma);
+    cos_gamma = cos(gamma);
+
+    sin_psi = sin(psi);
+    cos_psi = cos(psi);
+
+    sin_theta = sin(theta);
+    cos_theta = cos(theta);
+
+    M = [cos_psi*cos_theta                                 sin_theta            -sin_psi*cos_theta
+        -cos_psi*sin_theta*cos_gamma + sin_psi*sin_gamma   cos_theta*cos_gamma   cos_psi*sin_gamma + sin_psi*sin_theta*cos_gamma
+         cos_psi*sin_theta*sin_gamma + sin_psi*cos_gamma  -cos_theta*sin_gamma   cos_psi*cos_gamma - sin_psi*sin_theta*sin_gamma]';
+end
---
+++ pssk2ssk.m
@@ -0,0 +1,11 @@
+function M = pssk2ssk(alpha, betta)
+    % http://lektsii.org/3-22923.html
+    sa = sin(alpha);
+    ca = cos(alpha);
+    sb = sin(betta);
+    cb = cos(betta);
+
+    M = [ ca*cb sa -ca*sb;
+         -sa*cb ca  sa*sb;
+          sb     0  cb]';
+end
