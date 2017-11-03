10b9050998ff97f96e04e838c85037c12ef86220
AkhramovichSA <akhramovichsa@gmail.com>
2017-11-03 23:17:33 +0300

    euler angle xyz -> yzx

--- dq_from_euler_translation.m
+++ dq_from_euler_translation.m
@@ -1,6 +1,6 @@
 function dqout = dq_from_euler_translation(euler, translation)
 % DUALQUAT_FROM_EULER_TRANSLATION Return rotation quaterion from dual quaternion.
-% euler - vector 1x3 [roll, yaw, pitch] or [gamma, psi, theta], 
+% euler - vector 1x3 [yaw, pitch, roll] or [psi, theta, gamma], 
 % angle aroud [OX OY OZ]
 % translation - vector 1x3 [x, y, z]
 
@@ -8,7 +8,7 @@ dqout = zeros(1,8);
 dqout(1) = 1;
 
 % real part
-q_real = angle2quat(euler(1), euler(2), euler(3), 'xyz'); 
+q_real = angle2quat(euler(1), euler(2), euler(3), 'yzx'); 
 
 % dual part
 q_dual = quatmultiply([0 translation], q_real)/2;
--- dq_get_rotation_euler.m
+++ dq_get_rotation_euler.m
@@ -1,4 +1,4 @@
-function [angle_x, angle_y, angle_z] = dq_get_rotation_euler(dq)
+function [angle_y, angle_z, angle_x] = dq_get_rotation_euler(dq)
 % DUALQUAT_GET_ROTATION Return rotation quaterion from dual quaternion.
 
-[angle_x, angle_y, angle_z] = quat2angle(dq(1:4), 'xyz');
+[angle_y, angle_z, angle_x] = quat2angle(dq(1:4), 'yzx');
