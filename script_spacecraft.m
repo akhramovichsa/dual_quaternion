bc39756b3cc1896da974a74628025125666939cf
AkhramovichSA <akhramovichsa@gmail.com>
2018-04-14 02:35:46 +0300

    dynamic spacecraft and graphics

--- script_spacecraft.m
+++ script_spacecraft.m
@@ -1,18 +1,13 @@
 function script_spacecraft()
 clc; clear all;
+addpath('orb','-end');
 
-q_tek
-delta_q -> V -> P -> 
-q_kon
-
+global c;
+c.EARTH_MU = 398600.4415; % Геоцентрическая гравитационная постоянная, [км^3/c^2]
+c.EARTH_R0 = 6378;
 
 % Начальные условия полжения и ориентации КА
-rx      = 0;
-ry      = 0;
-rz      = 0;
-Vx      = 10;
-Vy      = 0;
-Vz      = 0;
+rv = orb_KeplerToXYZ(c.EARTH_R0 + 622, 0.0, deg2rad(30.0), deg2rad(0.0), deg2rad(0.0), c.EARTH_MU);
 
 psi     = deg2rad(0);
 theta   = deg2rad(0);
@@ -22,19 +17,50 @@ omega_y = deg2rad(0);
 omega_z = deg2rad(0);
 
 % Перевод в бикватернионную форму
-dual_omega = [0 omega_x omega_y omega_z  0 Vx Vy Vz];                  % Начальная угловая и линейная скорости
-dual_q     = dq_from_euler_translation([psi theta gamma], [rx ry rz]); % Начальная ориентация и положение ЛА
+dual_omega = [0 omega_x omega_y omega_z  0 rv(4:6)'];                  % Начальная угловая и линейная скорости
+dual_q     = dq_from_euler_translation([psi theta gamma], rv(1:3)'); % Начальная ориентация и положение ЛА
 
 % Интегрирование системы
-options = odeset('RelTol', 2e-2);
-[t, x] = ode45(@dx_dt, [0:0.5:50], [dual_omega, dual_q], options);
+options = odeset('RelTol', 2e-6);
+[t, x] = ode45(@dx_dt, [0:10.0:2*3600], [dual_omega, dual_q], options);
 
 d_omega = x(:, 1:8);
-d_q     = x(:, 9:16)
+d_q     = x(:, 9:16);
+
+
+figure(1)
+% clf
+x_max = 550;
+y_max = 130;
+% axis([0 x_max -30 y_max])
+hold on
+grid on
+% figure('Color', [1 1 1]);
+xlabel('\it r_x ,  м');
+ylabel('\it r_z ,  м');
+zlabel('\it r_y ,  м');
+for i = 1:1:length(d_q)
+    r                 = dq_get_translation_vector(d_q(i,:)); % Положение центра масс
+    rr(i, :) = r;
+    
+    plot3(r(1), r(2), r(3), 'k.'); % , LineStyle, '--', LineWidth, 1.0);
+end
+
+[x, y, z] = sphere;
+
+surf(c.EARTH_R0.*x, c.EARTH_R0.*y, c.EARTH_R0.*z);
+
+line_length = c.EARTH_R0 + 100;
+line([-line_length, line_length], [0, 0],                      [0, 0],                      'Color', [1 0 0]);
+line([0 0 ],                      [-line_length, line_length], [0, 0],                      'Color', [0 1 0]);
+line([0, 0],                      [0, 0],                      [-line_length, line_length], 'Color', [0 0 1]);
+
 
 end
 
 function dx_dt = dx_dt(t, x)
+global c;
+
 dual_omega = x(1:8)';  % Бикватернион угловой и линейной скоростей
 dual_q     = x(9:16)'; % Бикватернион положения ЛА вокруг центра масс и центра масс
 
@@ -47,8 +73,7 @@ r                 = dq_get_translation_vector(dual_q); % Радиус-вектор положения
 % -------------------------------------------------------------------------
 % Константы и характеристики КА
 % -------------------------------------------------------------------------
-MU_EARTH = 398600.4415; % Геоцентрическая гравитационная постоянная, [км^3*c^(?2)]
-mass     = 100;         % Масса КА, [кг]
+mass     = 1;         % Масса КА, [кг]
 
 % -------------------------------------------------------------------------
 % Тензор инерции
@@ -68,14 +93,13 @@ dual_J = [  Jq       zeros(4);
 % -------------------------------------------------------------------------
 % Гравитационная сила
 % -------------------------------------------------------------------------
-
-Fg_nsk = -MU_EARTH*[r(1) r(2) r(3)]/norm(r)^3;
+Fg_nsk = -c.EARTH_MU*r/(norm(r)^3);
 % Fg     = Fg_nsk*nsk2ssk(gamma, psi, theta);
 
 % -------------------------------------------------------------------------
 % Сумма сил
 % -------------------------------------------------------------------------
-dual_F = [0 Fg_nsk 0 0 0 0];
+dual_F = [0 0 0 0 0 Fg_nsk];
 
 % -------------------------------------------------------------------------
 % Уравнения движения
